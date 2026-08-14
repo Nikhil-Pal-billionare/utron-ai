95cc59e4c487aebedbdb391da2ba29d9e8175814
Nikhil-Pal-billionare <skrolindia@gmail.com>
2026-08-14 07:12:07 -0700

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -1,6 +1,10 @@
 export default async function handler(req, res) {
   if (req.method !== 'POST') return res.status(405).end();
 
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
   const response = await fetch('https://api.anthropic.com/v1/messages', {
     method: 'POST',
     headers: {
